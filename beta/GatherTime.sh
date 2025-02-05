fc46bd6f28eec2e26965577a9ca12094d42d31da
younjunsu <junsu_youn@naver.com>
2025-02-05 16:32:00 +0900

    Update GatherTime.sh

--- beta/GatherTime.sh
+++ beta/GatherTime.sh
@@ -3,10 +3,13 @@
 # DD-MMThh24:mi:ss
 
 
-[11-28T10:41:34
-[2024-11-30T04:15:12
+
+
 
 logfile=yearno
 logfile=yearyes
+
+# [11-28T10:41:34
 grep -E '^\[?[0-9]{4}-' "$logfile" > year_yes_logs.txt
+# [2024-11-30T04:15:12
 grep -E '^\[?[0-9]{2}-' "$logfile" | grep -vE '^\[?[0-9]{4}-' > year_no_logs.txt
